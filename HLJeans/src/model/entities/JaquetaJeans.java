4f41fc019b659d89f31aa8d37d7cf7be269692c3
JHugoRodrigues <hugo.rsouza@upe.br>
2025-11-26 18:54:33 -0300

    Delete HLJeans/src directory

--- HLJeans/src/model/entities/JaquetaJeans.java
+++
@@ -1,9 +0,0 @@
-package model.entities;
-
-public class JaquetaJeans extends ProdutoAbstrato{
-
-	public JaquetaJeans(String nome, int qtdEstoque, double preco) {
-		super(nome, qtdEstoque, preco);
-	}
-
-}
